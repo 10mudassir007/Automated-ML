be9ed031fe96c553be1e7f504cd30fa4e799b536
10mudassir007 <muddassir032@gmail.com>
2025-03-08 22:09:37 +0500

    predict

--- app.py
+++ app.py
@@ -300,5 +300,5 @@ if submitted:
     st.write(df_processed.columns)
     st.write(model.get_booster().feature_names)
     #st.write(model.predict(df_processed.iloc[0]))
-    st.write(f"#### Predicted Value: {round(model.predict(processed_input.to_numpy()),2)}",unsafe_allow_html=True)
+    st.write(f"#### Predicted Value: {model.predict(processed_input.to_numpy())}",unsafe_allow_html=True)
     
